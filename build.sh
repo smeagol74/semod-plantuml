ef540b393ebd9fbf2413bf76cc1d225706c94351
Konstantin V. Borisov <smeagol74@gmail.com>
2019-11-13 00:20:29 +0700

    + built dist

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+DIST=./dist
+SRC=./src
+
+function build() {
+  SCRIPT=$1
+  PARTS=$(cat ${SRC}/${SCRIPT} | sed 's/!include //')
+  touch ${DIST}/${SCRIPT}
+  for PART in ${PARTS}; do
+    cat ${SRC}/${PART} >> ${DIST}/${SCRIPT}
+    echo '' >> ${DIST}/${SCRIPT}
+    echo '' >> ${DIST}/${SCRIPT}
+  done
+}
+
+rm -rf ${DIST}
+mkdir ${DIST}
+
+build semod.puml
+build archimate.puml
+build system.puml
