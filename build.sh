75bd4c20815daa0f5e67d436bef52bb90f183ee5
Konstantin Borisov <smeagol74@gmail.com>
2023-09-20 21:53:50 +0700

    upgrade syntax to !procedure

--- build.sh
+++ build.sh
@@ -5,20 +5,21 @@ SRC=./src
 
 function build() {
   SCRIPT=$1
+  DEST=$2
   PARTS=$(cat ${SRC}/${SCRIPT} | sed 's/!include //')
-  touch ${DIST}/${SCRIPT}
+  touch ${DIST}/${DEST}
   for PART in ${PARTS}; do
-    cat ${SRC}/${PART} >> ${DIST}/${SCRIPT}
-    echo '' >> ${DIST}/${SCRIPT}
-    echo '' >> ${DIST}/${SCRIPT}
+    cat ${SRC}/${PART} >> ${DIST}/${DEST}
+    echo '' >> ${DIST}/${DEST}
+    echo '' >> ${DIST}/${DEST}
   done
 }
 
 rm -rf ${DIST}
 mkdir ${DIST}
 
-build semod.iuml
-build archimate.iuml
-build system.iuml
-build ontology.iuml
-build essence.iuml
+build semod.iuml semod.puml
+build archimate.iuml archimate.puml
+build system.iuml system.puml
+build ontology.iuml ontology.puml
+build essence.iuml essence.puml
